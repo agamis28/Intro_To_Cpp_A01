ef02ca2ffb4b424bd347dec28dc8c4ed9ceca307
Angel Gamis <agamis@rrc.ca>
2024-09-04 12:23:23 -0500

    Adding header and utlis to part 4

---
+++ Part4_GuessThatNumber/utlis.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <limits>
+
+// Foward Declaring Functions
+
+void clearCin();
+
+bool checkIfNumber(std::string);
+
+int pickLowBoundsOfGame();
+
+int pickHighBoundsOfGame();
+
+int pickRandomInt(int, int);
+
+void playGame();
